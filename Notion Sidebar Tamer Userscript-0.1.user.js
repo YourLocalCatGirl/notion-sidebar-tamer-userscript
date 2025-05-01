fbcfb091b6c72a1d4e6291c1f924e4872f78c471
Sari Kočí <140644392+YourLocalCatGirl@users.noreply.github.com>
2025-05-01 20:37:29 +0200

    Update and rename content.js to Notion Sidebar Tamer Userscript-0.1.user.js
    changing the code for tampermonkey

--- Notion Sidebar Tamer Userscript-0.1.user.js
+++ Notion Sidebar Tamer Userscript-0.1.user.js
@@ -1,3 +1,14 @@
+// ==UserScript==
+// @name         Notion Sidebar Tamer Userscript
+// @icon       https://github.com/cyanxiao/notion-sidebar-tamer/blob/main/images/128.png
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Disable the hover sidebar in Notion. The sidebar in Notion is a great way to navigate between pages, but it can be distracting when you're working between windows. This Chrome extension disables the appearing on hover of the sidebar.
+// @author       yourlocalgirl
+// @match        https://www.notion.so/*
+// @grant        none
+// ==/UserScript==
+
 const sidebarInitObserver = new MutationObserver((mutationsList, observer) => {
   const targetElement = document.querySelector(".notion-sidebar");
   if (targetElement) {
@@ -10,9 +21,7 @@ const sidebarInitObserver = new MutationObserver((mutationsList, observer) => {
     });
   }
 });
-
 let previousExpanded = null;
-
 function expandedObserverCallback(mutationsList, observer) {
   const targetElement = document.querySelector(".notion-sidebar");
   const sidebarData = JSON.parse(
@@ -23,5 +32,4 @@ function expandedObserverCallback(mutationsList, observer) {
     targetElement.style.display = expanded ? "block" : "none";
   }
 }
-
 sidebarInitObserver.observe(document.body, { subtree: true, childList: true });
